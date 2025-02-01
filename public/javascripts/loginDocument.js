dcb4a294aaa2b1a3f93014e1547559fc15f5db06
nico.biesinger <nico.biesinger@porsche.digital>
2025-02-01 17:14:23 +0100

    Anmeldeformular wurde jetzt um eine Benutzerprüfung addiert.

--- public/javascripts/loginDocument.js
+++ public/javascripts/loginDocument.js
@@ -1,3 +1,15 @@
-function sendFormula(id) {
-    console.log(document.getElementById(id).value);
+function sendFormula(username_id,password_id) {
+    const username_value = document.getElementById(username_id).value;
+    const password_value = document.getElementById(password_id).value;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "/users/login", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log("Login details sent successfully");
+        }
+    };
+    xhr.send(JSON.stringify({ username: username_value, password: password_value }));
 }
+
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,54 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var fs = require("fs");
+var path = require("path");
+const {log} = require("debug");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+    res.send("respond with a resource");
+});
+
+/* POST login details */
+router.post("/login", function (req, res, next) {
+    const loginDetails = req.body;
+    const filePath = path.join(__dirname, "../data/loginDetails.json");
+
+    fs.readFile(filePath, "utf8", (err, data) => {
+        if (err && err.code !== "ENOENT") {
+            return next(err);
+        }
+
+        let loginData = [];
+        if (data) {
+            try {
+                loginData = JSON.parse(data);
+            } catch (parseErr) {
+                return next(parseErr);
+            }
+        }
+        if (proofOfExistingUser(loginData, loginDetails)) {
+            console.log("Der User " + loginDetails.username + " existiert bereits.")
+        } else {
+            loginData.push(loginDetails);
+        }
+        fs.writeFile(filePath, JSON.stringify(loginData, null, 2), (err) => {
+            if (err) {
+                return next(err);
+            }
+            res.status(200).send("Login details saved successfully");
+        });
+    });
 });
 
 module.exports = router;
+
+function proofOfExistingUser(loginData, loginDetails) {
+    for (let user of loginData) {
+        if (loginDetails.username === user.username) {
+            console.log(user.username)
+            return true;
+        }
+    }
+    return false;
+}
